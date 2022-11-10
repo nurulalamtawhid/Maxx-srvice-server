d4d3a5afcc39538aa3613c26e6640db68a098349
tawhidhaider <tawhidalam69@gmail.com>
2022-11-10 23:57:47 +0600

    service api & mongo db

---
+++ index.js
@@ -0,0 +1,52 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const express = require('express');
+const cors = require ('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+require('dotenv').config();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+//db
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_KEY}@database1.wijxnwd.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        const serviceCollection = client.db("maxxService").collection("Services");
+        //console.log(serviceCollection);
+
+
+
+        //api for services
+        app.get('/services', async(req,res)=>{
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        
+        });
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(error => console.error(error));
+
+
+app.get('/',(req,res)=>{
+    res.send('server running on')
+})
+
+app.listen(port,()=>{
+    console.log(`server running on port ${port}`);
+})
